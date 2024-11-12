7097c681a42a4f3423dbfa78f50a461f8481f272
Andy Simota <andyiulian.simota@gmail.com>
2024-11-12 15:26:40 +0100

    created an array of objects and printed the lightest bike

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-/* Snack 1
-Creare un array di oggetti:
-Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore.
-Snack2
+/* Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -11,3 +7,38 @@ Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subit
 Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. */
+
+// Snack 1
+const bikes = [
+  {
+    brand: "scott",
+    weight: 120005,
+  },
+  {
+    brand: "bianchi",
+    weight: 132,
+  },
+  {
+    brand: "cannondale",
+    weight: 1221,
+  },
+  {
+    brand: "trek",
+    weight: 12229,
+  },
+];
+
+let lightestBike = bikes[0];
+for (let i = 0; i < bikes.length; i++) {
+  if (bikes[i].weight < lightestBike.weight) {
+    lightestBike = bikes[i];
+  }
+}
+console.log(lightestBike);
+
+// Snack 2
+const teams = [
+  {
+    team: "Milan",
+  },
+];
